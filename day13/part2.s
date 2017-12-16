57499bac6d111a1d881bff85838c6a0d7caf4db2
Lonami Exo <totufals@hotmail.com>
2017-12-16 17:51:22 +0100

    Fix day13/part2 using modular math
    Instead moving 100 scanners thousands of time every time a new
    delay was set (could actually even rewind to be faster), just
    use modular math and don't store their positions at all. There
    is no need to even calculate its real position, only if they
    are at the first row.

--- day13/part2.s
+++ day13/part2.s
@@ -3,8 +3,6 @@
 .data
 	.set N, 100  #; amount of scanners
 	ranges: .zero N  #; the range of scanner[i]
-	scanners: .zero N  #; the current position of scanners
-	updown: .space N, 1  #; moving up (+1) or down (-1), flipped on bounds
 
 	fmt: .string "%d\n"
 	readn: .string "%d: %d"
@@ -13,33 +11,6 @@
 
 .text
 	.global main
-
-#; r12 points to the base of ranges
-#; rsi points to the base of scanners
-#; rdi points to the base of updown
-#; uses r9, r10
-movescanners:
-	mov rcx, N
-ms_loop:
-	mov r9b, -1[r12+rcx]   #; r9b  = range - 1
-	mov r10b, -1[rsi+rcx]  #; r10b = scanner position
-	test r9b, r9b
-	jz ms_nextiter  #; important: no range, do nothing
-	dec r9b
-	add r10b, -1[rdi+rcx]  #; moving up or down?
-	#; check bounds and swap if necessary
-	cmp r10b, r9b
-	je ms_swapdir
-	test r10b, r10b
-	jz ms_swapdir
-	jmp ms_nextiter
-ms_swapdir:
-	neg byte ptr -1[rdi+rcx]  #; swap direction in memory
-ms_nextiter:
-	mov -1[rsi+rcx], r10b  #; save new position
-	loop ms_loop
-	ret
-
 
 main:
 	push r12
@@ -59,46 +30,41 @@ readloop:
 	jmp readloop
 
 readdone:
-	lea rsi, scanners[rip]
-	lea rdi, updown[rip]
 
-	xor rax, rax  #; current delay
+	xor r9, r9  #; current delay
 startwalk:
 	xor r8, r8  #; current index
 walkloop:
-	#; we walk on the 0'th line, so if it's not zero scanner isn't here
-	test byte ptr [rsi+r8], 0xff
-	jz caught
-fakecaught:
-	call movescanners
+	xor rcx, rcx
+	mov cl, [r12+r8]
+	test cl, cl
+	jz walkloopnext  #; no range, nothing to do
+	dec rcx
+	shl rcx
+	#; the position is current time % ((range - 1) * 2), e.g.
+	#; 2 -> 2;    3 -> 4;    4 -> 6;    5 -> 8;    and so on.
+	xor rdx, rdx
+	mov rax, r8  #; current time = index + delay
+	add rax, r9
+	div rcx
+	#; we have modulo in rdx now.
+	#; we actually don't need to do X = (mod - reminder) if reminder > range
+	#; to know its exact position, if not, it's outside the real range mod.
+	#; we just need to know if it's 0.
+	test rdx, rdx
+	jnz walkloopnext  #; if it's not zero, we weren't caught
+	#; otherwise we were caught and need to restart
+	inc r9
+	jmp startwalk
+
+walkloopnext:
 	inc r8
 	cmp r8, 100
-	je show  #; we completed it and didn't get caught!
-
-caught:
-	#; we might not be caught, simply there was no range
-	test byte ptr [r12+r8], 0xff
-	jz fakecaught
-	#; there was a range, we really got caught, incremenet skip
-	inc rax
-	#; reset scanners and their sign
-	mov rcx, N
-caughtclean:
-	#; could rep stosb but we need to save in two positions anyway
-	mov byte ptr -1[rsi+rcx], 0
-	mov byte ptr -1[rdi+rcx], 1
-	loop caughtclean
-	#; advance scanners delay times (rcx is used by movescanners)
-	mov rdx, rax
-caughtdelay:
-	call movescanners
-	dec rdx
-	jnz caughtdelay
-	jmp startwalk
+	jne walkloop
 
 show:
 	lea rdi, fmt[rip]
-	mov rsi, rax
+	mov rsi, r9
 	xor rax, rax
 	call printf@PLT
 
