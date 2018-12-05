7ecfdb4a018957aa9bfbc71569b23dda600505d1
Lonami Exo <totufals@hotmail.com>
2018-12-05 09:25:18 +0100

    Add day05/part2

--- year2018/day05/src/main.rs
+++ year2018/day05/src/main.rs
@@ -2,29 +2,50 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 
-fn part1() -> i32 {
-    let mut data = BufReader::new(File::open("input").expect("failed to open"))
+fn read_input() -> Vec<u8> {
+    BufReader::new(File::open("input").expect("failed to open"))
         .lines()
         .next()
         .expect("empty file")
         .expect("failed to read")
-        .into_bytes();
+        .into_bytes()
+}
 
+fn react(mut data: Vec<u8>) -> Vec<u8> {
     let mut any = true;
     while any {
         any = false;
-        for i in (0..data.len() - 1).rev() {
-            if (data[i + 1] as i32 - data[i] as i32).abs() == 32 {
+        let mut i = data.len();
+        while i > 1 {
+            i -= 1;
+            if (data[i] as i32 - data[i - 1] as i32).abs() == 32 {
                 any = true;
                 data.remove(i);
+                i -= 1;
                 data.remove(i);
             }
         }
     }
+    data
+}
 
-    data.len() as i32
+fn part1(data: &Vec<u8>) -> i32 {
+    react(data.clone()).len() as i32
+}
+
+fn part2(data: &Vec<u8>) -> i32 {
+    (b'a'..=b'z').map(|skip| {
+        react(data
+              .iter()
+              .filter(|&&x| x != skip && x != skip - 32)
+              .map(|&x| x)
+              .collect()
+              ).len() as i32
+    }).min().expect("rust ranges are broken")
 }
 
 fn main() {
-    println!("{}", part1());
+    let input = read_input();
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
