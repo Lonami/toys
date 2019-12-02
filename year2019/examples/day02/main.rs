abbcf6390cf5480a28e07668ddbdd1b14fd78cac
Lonami Exo <totufals@hotmail.com>
2019-12-02 11:22:08 +0100

    Add one-liner solution for 2019/01/1

--- year2019/examples/day02/main.rs
+++ year2019/examples/day02/main.rs
@@ -84,6 +84,24 @@ impl Program {
     }
 }
 
+// Part 1 Python one-liner:
+// (lambda s, *a: s(s, *a))(lambda s, a, r, m, pc: s(s, a, r, a(m, m[pc + 3], r(m, pc + 1) + r(m, pc + 2)), pc + 4) if m[pc] == 1 else s(s, a, r, a(m, m[pc + 3], r(m, pc + 1) * r(m, pc + 2)), pc + 4) if m[pc] == 2 else m[0] if m[pc] == 99 else s(s, a, r, m, pc + 1), lambda m, i, v: m[:i] + [v] + m[i+1:], lambda m, i: m[m[i]], (lambda m: m[:1] + [12, 2] + m[3:])(list(map(int, open('input').read().split(',')))), 0)
+//
+// Non-stupid indent and proper names:
+// (lambda step, *args: step(step, *args))(lambda step, assign, read, memory, pc:
+//     step(step, assign, read, assign(memory, memory[pc + 3], read(memory, pc + 1) + read(memory, pc + 2)), pc + 4)
+//         if memory[pc] == 1 else
+//     step(step, assign, read, assign(memory, memory[pc + 3], read(memory, pc + 1) * read(memory, pc + 2)), pc + 4)
+//         if memory[pc] == 2 else
+//     memory[0]
+//         if memory[pc] == 99 else
+//     step(step, assign, read, memory, pc + 1),
+//     lambda m, i, v: m[:i] + [v] + m[i+1:],
+//     lambda m, i: m[m[i]],
+//     (lambda m: m[:1] + [12, 2] + m[3:])(list(map(int, open('input').read().split(',')))),
+//     0
+// )
+
 fn main() {
     let mut program = Program::new();
     program.save();
