130410811f9752d849ab1af5ad67a2b02bc78d4e
Lonami Exo <totufals@hotmail.com>
2019-12-04 14:33:26 +0100

    Add 2019/04/2

--- year2019/examples/day04/main.rs
+++ year2019/examples/day04/main.rs
@@ -22,15 +22,19 @@ fn read_input_range() -> (i32, i32) {
     (first.max(100000), second.min(999999))
 }
 
-fn is_password_valid(password: i32) -> bool {
-    let digits = [
+fn get_digits(password: i32) -> [i32; 6] {
+    [
         (password / 100000) % 10,
         (password / 10000) % 10,
         (password / 1000) % 10,
         (password / 100) % 10,
         (password / 10) % 10,
         (password / 1) % 10
-    ];
+    ]
+}
+
+fn is_password_valid(password: i32) -> bool {
+    let digits = get_digits(password);
 
     // Two adjacent digits are the same (like 22 in 122345).
     // Going from left to right, the digits never decrease.
@@ -38,7 +42,32 @@ fn is_password_valid(password: i32) -> bool {
         && digits.windows(2).all(|w| w[0] <= w[1])
 }
 
+fn is_second_password_valid(password: i32) -> bool {
+    let digits = get_digits(password);
+
+    // Going from left to right, the digits never decrease.
+    if !digits.windows(2).all(|w| w[0] <= w[1]) {
+        return false;
+    }
+
+    let mut pair_count = 0;
+    let mut last_digit = digits[0];
+    for &digit in digits.iter().skip(1) {
+        if digit == last_digit {
+            pair_count += 1;
+        } else if pair_count == 1 {
+            break;
+        } else {
+            pair_count = 0;
+            last_digit = digit;
+        }
+    }
+
+    pair_count == 1
+}
+
 fn main() {
     let (lo, hi) = read_input_range();
     println!("{}", (lo..=hi).filter(|x| is_password_valid(*x)).count());
+    println!("{}", (lo..=hi).filter(|x| is_second_password_valid(*x)).count());
 }
