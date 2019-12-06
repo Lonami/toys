ce2c33abc072d99fe63ec994e09652b6acfca39e
Lonami Exo <totufals@hotmail.com>
2019-12-06 12:53:14 +0100

    Optimize 2019/06

--- year2019/examples/day06/main.rs
+++ year2019/examples/day06/main.rs
@@ -1,68 +1,95 @@
+use std::collections::HashMap;
 use std::io::{stdin, BufRead};
+use std::fmt;
 
-#[derive(Debug)]
-struct Orbit {
-    center: String,
-    orbits: String  // `orbits` is in orbit around `center`
-}
+#[derive(PartialEq, Eq, Clone, Copy, Hash)]
+struct Planet(i32);
 
-impl Orbit {
-    fn from_str(string: &str) -> Self {
-        let mut it = string.split(')');
-        let center = it.next().expect("empty input").into();
-        let orbits = it.next().expect("malformed input lacks ')'").into();
-        Self { center, orbits }
+impl std::convert::From<&str> for Planet {
+    fn from(string: &str) -> Self {
+        let bytes = string.as_bytes();
+        Self(i32::from_ne_bytes([
+            0u8,
+            *bytes.get(0).unwrap_or(&0u8),
+            *bytes.get(1).unwrap_or(&0u8),
+            *bytes.get(2).unwrap_or(&0u8)
+        ]))
     }
 }
 
-fn read_input() -> Vec<Orbit> {
-    stdin()
-        .lock()
-        .lines()
-        .map(|line| line.expect("error while reading input file"))
-        .map(|line| Orbit::from_str(&line))
-        .collect()
+impl fmt::Display for Planet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for byte in self.0.to_ne_bytes().iter().filter(|b| **b != 0u8) {
+            write!(f, "{}", *byte as char)?;
+        };
+        Ok(())
+    }
 }
 
-fn count_orbits(orbits: &Vec<Orbit>, center: &str, depth: i32) -> i32 {
+impl fmt::Debug for Planet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Planet({})", self)
+    }
+}
+
+type OrbitMap = HashMap<Planet, Vec<Planet>>;
+
+fn parse_planet_pair(string: &str) -> (Planet, Planet) {
+    let mut it = string.split(')');
+    let center = it.next().expect("empty input").into();
+    let orbits = it.next().expect("malformed input lacks ')'").into();
+    (center, orbits)
+}
+
+fn read_input() -> OrbitMap {
+    let mut result = OrbitMap::new();
+    for (center, orbits) in stdin()
+            .lock()
+            .lines()
+            .map(|line| line.expect("error while reading input file"))
+            .map(|line| parse_planet_pair(&line)) {
+        result.entry(center).or_insert(vec![]).push(orbits);
+    }
+    result
+}
+
+fn count_orbits(orbits: &OrbitMap, center: Planet, depth: usize) -> usize {
     // We have `1` direct parent plus `(depth - 1)` indirect parents,
     // for a total of `depth` parents. Plus, we need to count the orbits
     // for all of our children (what is orbitting around our center).
     //
     // If the depth is 0, it means we actually don't have parents, so
     // it cancels out with our assumption of one direct parent nicely.
-    let mut result = depth;
-    for orbit in orbits {
-        // TODO this is yelling for a Map
-        // We also could store the integers inside a single INT
-        if orbit.center == center {
-            result += count_orbits(orbits, &orbit.orbits, depth + 1);
-        }
-    }
-
-    result
+    depth + orbits
+        .get(&center)
+        .unwrap_or(&vec![])
+        .iter()
+        .map(|planet| count_orbits(orbits, *planet, depth + 1))
+        .sum::<usize>()
 }
 
-fn find_path<'a>(orbits: &'a Vec<Orbit>, center: &str, goal: &str) -> Option<Vec<&'a str>> {
-    for orbit in orbits {
-        if orbit.center == center {
-            if orbit.orbits == goal {
-                return Some(vec![&orbit.orbits, &orbit.center]);
-            } else {
-                if let Some(mut result) = find_path(orbits, &orbit.orbits, goal) {
-                    result.push(&orbit.center);
-                    return Some(result);
-                }
+fn find_path(orbits: &OrbitMap, center: Planet, goal: Planet) -> Option<Vec<Planet>> {
+    if let Some(orbitting) = orbits.get(&center) {
+        for orbit in orbitting.iter() {
+            if *orbit == goal {
+                return Some(vec![center, *orbit]);
+            } else if let Some(mut result) = find_path(orbits, *orbit, goal) {
+                result.push(center);
+                return Some(result);
             }
         }
     }
     None
 }
 
-fn count_distance(orbits: &Vec<Orbit>, center: &str, src: &str, dst: &str) -> i32 {
+fn count_distance(orbits: &OrbitMap, center: Planet, src: Planet, dst: Planet) -> i32 {
+    if src == dst {
+        return 0;
+    }
+
     // Generate a list of parents to `src` and to `dst`
-    let mut src_parents = find_path(&orbits, center, src).expect(&format!("no route from {} to {}", center, src));
-    let mut dst_parents = find_path(&orbits, center, dst).expect(&format!("no route from {} to {}", center, dst));
+    let src_parents = find_path(orbits, center, src).expect(&format!("no route from {} to {}", center, src));
+    let dst_parents = find_path(orbits, center, dst).expect(&format!("no route from {} to {}", center, dst));
 
     // Determine at which point they start differing. This means all
     // previous parents are common, and the rest different, so we just
@@ -74,11 +101,16 @@ fn count_distance(orbits: &Vec<Orbit>, center: &str, src: &str, dst: &str) -> i3
         }
     }
 
-    -1
+    // We start at the same center and src != dst, it's impossible the paths from
+    // center to src and dst don't differ if we found a route (the `expect` above).
+    unreachable!();
 }
 
 fn main() {
+    let com: Planet = "COM".into();
+    let you: Planet = "YOU".into();
+    let san: Planet = "SAN".into();
     let orbits = read_input();
-    println!("{}", count_orbits(&orbits, "COM", 0));
-    println!("{}", count_distance(&orbits, "COM", "YOU", "SAN"));
+    println!("{}", count_orbits(&orbits, com, 0));
+    println!("{}", count_distance(&orbits, com, you, san));
 }
