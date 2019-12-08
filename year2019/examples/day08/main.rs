bf33abeae7173b68c8b44b9e374d84675c8b1cdf
Lonami Exo <totufals@hotmail.com>
2019-12-08 10:27:21 +0100

    Add 2019/08/2

--- year2019/examples/day08/main.rs
+++ year2019/examples/day08/main.rs
@@ -42,6 +42,19 @@ impl Image {
     fn layer(&self, index: usize) -> &[u8] {
         &self.pixels[self.area() * index..self.area() * (index + 1)]
     }
+
+    // 0 is black, 1 is white, and 2 is transparent.
+    // first layer in front and the last layer in back
+    fn compose_layers(&self) -> Vec<u8> {
+        (0..self.area())
+            .map(|pi| {
+                (0..self.layer_count())
+                    .map(|li| self.layer(li)[pi])
+                    .find(|pixel| *pixel != 2u8)
+                    .unwrap_or(2)
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -62,4 +75,12 @@ fn main() {
                 .filter(|pixel| **pixel == 2)
                 .count()
     );
+
+    let composed = image.compose_layers();
+    for y in 0..image.height {
+        for x in 0..image.width {
+            print!("{}", if composed[y * image.width + x] == 1 { "██" } else { "  " });
+        }
+        println!();
+    }
 }
