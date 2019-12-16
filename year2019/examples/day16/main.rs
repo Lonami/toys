6ceb4d54297082453f711c093d3a4b1a037d9b43
Lonami Exo <totufals@hotmail.com>
2019-12-16 11:36:57 +0100

    Add 2019/16/2

--- year2019/examples/day16/main.rs
+++ year2019/examples/day16/main.rs
@@ -1,6 +1,7 @@
 use std::io::{stdin, Read};
 
 const BASE_PATTERN: [i64; 4] = [0, 1, 0, -1];
+const REAL_SIGNAL_MULTIPLIER: usize = 10000;
 
 fn read_input() -> Vec<u8> {
     let mut buffer = Vec::new();
@@ -42,18 +43,60 @@ fn fft_phase(digits: &Vec<u8>) -> Vec<u8> {
         .collect()
 }
 
-fn main() {
-    let mut digits = read_input();
-    for _ in 0..100 {
-        digits = fft_phase(&digits);
+fn late_fft_phase(digits: &Vec<u8>) -> Vec<u8> {
+    // We start with all the remaining digits being multiplied by 1 (so it's their sum).
+    // Every next digit just has the previous ones as 0, which is why we constantly subtract.
+    // TODO This can probably be made more fancy with a fold of some sort.
+    let mut result = Vec::with_capacity(digits.len());
+    let mut sum: usize = digits.iter().map(|digit| *digit as usize).sum();
+    for d in digits {
+        result.push((sum % 10) as u8);
+        sum -= *d as usize;
     }
+    result
+}
 
+fn show_signal(signal: &Vec<u8>) {
     println!(
         "{}",
-        digits
+        signal
             .iter()
             .take(8)
             .map(|d| (d + b'0') as char)
             .collect::<String>()
     );
+}
+
+fn main() {
+    let original_signal = read_input();
+    let mut signal = original_signal.clone();
+    for _ in 0..100 {
+        signal = fft_phase(&signal);
+    }
+    show_signal(&signal);
+
+    let signal = original_signal.clone();
+    let mut offset = 0;
+    for digit in signal.iter().take(7) {
+        offset = (offset * 10) + *digit as usize;
+    }
+
+    // Because our solution is at `offset` we can `skip` all the initial values,
+    // since the pattern would be `0` for these digits and thus never contribute.
+    let mut real_signal: Vec<u8> = signal
+        .iter()
+        .cycle()
+        .take(signal.len() * REAL_SIGNAL_MULTIPLIER)
+        .skip(offset)
+        .map(|d| *d)
+        .collect();
+
+    // We will have `offset` ones for the first item, then `offset + 1` and so on.
+    // If the offset is larger than our remaining signal, that means we just have
+    // to add the digits (because they all multiply by one).
+    assert!(real_signal.len() < offset);
+    for _ in 0..100 {
+        real_signal = late_fft_phase(&real_signal);
+    }
+    show_signal(&real_signal);
 }
