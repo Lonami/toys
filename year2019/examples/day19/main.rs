298515c8632b9ec5f0405d16ba835f95f997d6d9
Lonami Exo <totufals@hotmail.com>
2019-12-19 10:49:09 +0100

    Simplify 2019/19/1

--- year2019/examples/day19/main.rs
+++ year2019/examples/day19/main.rs
@@ -1,64 +1,34 @@
-use std::convert::Into;
 use year2019::intcode::{Program, StepResult};
 
-struct Drone {
-    forces: Vec<Force>,
-}
+fn count_pulls(program: &mut Program, width: usize, height: usize) -> usize {
+    program.save();
+    let mut count = 0;
+    let mut index_it = (0..width * height).into_iter();
 
-#[derive(Clone, Copy, PartialEq)]
-enum Force {
-    Stationary = 0,
-    Pulled = 1,
-}
-
-impl Into<Force> for i64 {
-    fn into(self) -> Force {
-        match self {
-            0 => Force::Stationary,
-            1 => Force::Pulled,
-            _ => panic!(format!("cannot convert {} into a force", self)),
+    loop {
+        match program.step() {
+            StepResult::Continue => continue,
+            StepResult::NeedInput => {
+                if let Some(index) = index_it.next() {
+                    let x = index % width;
+                    let y = index / width;
+                    program.push_input(x as i32);
+                    program.push_input(y as i32);
+                } else {
+                    return count;
+                }
+            },
+            StepResult::Output(value) => {
+                count += value as usize;
+            },
+            StepResult::CaughtFire => {
+                program.reset();
+            },
         }
-    }
-}
-
-impl Drone {
-    fn new(program: &mut Program, width: usize, height: usize) -> Self {
-        program.save();
-
-        let mut forces = Vec::with_capacity(width * height);
-        let mut index_it = (0..width * height).into_iter();
-        loop {
-            match program.step() {
-                StepResult::Continue => continue,
-                StepResult::NeedInput => {
-                    if let Some(index) = index_it.next() {
-                        let x = index % width;
-                        let y = index / width;
-                        program.push_input(x as i32);
-                        program.push_input(y as i32);
-                    } else {
-                        break;
-                    }
-                },
-                StepResult::Output(value) => {
-                    forces.push(value.into());
-                },
-                StepResult::CaughtFire => {
-                    program.reset();
-                },
-            }
-        }
-
-        Self { forces }
-    }
-
-    fn count_affected_areas(&self) -> usize {
-        self.forces.iter().filter(|force| **force == Force::Pulled).count()
     }
 }
 
 fn main() {
     let mut program = Program::from_stdin();
-    let drone = Drone::new(&mut program, 50, 50);
-    println!("{}", drone.count_affected_areas());
+    println!("{}", count_pulls(&mut program, 50, 50));
 }
