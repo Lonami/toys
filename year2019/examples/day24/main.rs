1ac790ddb1687519a2f5592b8dcf2d23ee5eb7e6
Lonami Exo <totufals@hotmail.com>
2019-12-24 13:20:22 +0100

    Add non-working 2019/24/2

--- year2019/examples/day24/main.rs
+++ year2019/examples/day24/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 const ERIS_SIZE: usize = 5;
 
-#[derive(PartialEq, Debug)]
+#[derive(Clone, PartialEq, Debug)]
 enum Tile {
     Empty,
     Bug,
@@ -15,6 +15,15 @@ struct Eris {
     map: Vec<Tile>,
     width: usize,
     height: usize,
+}
+
+struct MultiEris {
+    map: Vec<Tile>,
+    layers: usize,
+    width: usize,
+    height: usize,
+    lo: usize,
+    hi: usize,
 }
 
 impl From<u8> for Tile {
@@ -116,8 +125,139 @@ impl fmt::Display for Eris {
     }
 }
 
+impl MultiEris {
+    fn new(base: &Eris, layers: usize) -> Self {
+        let area = base.height * base.width;
+        let mut map = vec![Tile::Empty; layers * area];
+        let lo = layers / 2;
+        let hi = lo;
+
+        map[(lo * area)..((1 + lo) * area)].clone_from_slice(&base.map[..]);
+        MultiEris {
+            map,
+            layers,
+            width: base.width,
+            height: base.height,
+            lo,
+            hi,
+        }
+    }
+
+    #[inline(always)]
+    fn infested(&self, layer: usize, y: usize, x: usize) -> usize {
+        (self.map[layer * (self.width * self.height) + y * (self.width) + x] == Tile::Bug) as usize
+    }
+
+    fn count_live_neighbours(&self, layer: usize, y: usize, x: usize) -> usize {
+        let midy = self.height / 2;
+        let midx = self.width / 2;
+        if y == midy && x == midx {
+            // The middle is always empty and never has direct neighbours
+            return 0;
+        }
+
+        let mut result = 0;
+
+        // Left
+        if x == 0 {
+            result += self.infested(layer - 1, midy, midx - 1);
+        } else if x == midx + 1 && y == midy {
+            for i in 0..self.height {
+                result += self.infested(layer - 1, i, self.width - 1);
+            }   
+        } else {
+            result += self.infested(layer, y, x - 1);
+        }
+
+        // Right
+        if x == self.width - 1 {
+            result += self.infested(layer - 1, midy, midx + 1);
+        } else if x == midx - 1 && y == midy {
+            for i in 0..self.height {
+                result += self.infested(layer - 1, i, 0);
+            }
+        } else {
+            result += self.infested(layer, y, x + 1);
+        }
+
+        // Up
+        if y == 0 {
+            result += self.infested(layer - 1, midy - 1, midx);
+        } else if y == midy + 1 && x == midx {
+            for j in 0..self.width {
+                result += self.infested(layer - 1, self.height - 1, j);
+            }
+        } else {
+            result += self.infested(layer, y - 1, x);
+        }
+
+        // Down
+        if y == self.height - 1 {
+            result += self.infested(layer - 1, midy, midx + 1);
+        } else if y == midy - 1 && x == midx {
+            for j in 0..self.width {
+                result += self.infested(layer - 1, 0, j);
+            }
+        } else {
+            result += self.infested(layer, y + 1, x);
+        }
+
+        result
+    }
+
+    fn tick(&mut self) {
+        let mut new = Vec::with_capacity(self.map.len());
+        for _ in 0..(self.width * self.height) {
+            new.push(Tile::Empty);
+        }
+        for layer in 1..(self.layers - 1) {
+            for y in 0..self.height {
+                for x in 0..self.width {
+                    let neighbours = self.count_live_neighbours(layer, y, x);
+                    new.push(match self.map[layer * (self.width * self.height) + y * (self.height) + x] {
+                        Tile::Empty => {
+                            if neighbours == 1 || neighbours == 2 {
+                                Tile::Bug
+                            } else {
+                                Tile::Empty
+                            }
+                        },
+                        Tile::Bug => {
+                            if neighbours == 1 {
+                                Tile::Bug
+                            } else {
+                                Tile::Empty
+                            }
+                        }
+                    });
+                }
+            }
+        }
+        for _ in 0..(self.width * self.height) {
+            new.push(Tile::Empty);
+        }
+        self.map = new;
+    }
+
+    fn count_live(&self) -> usize {
+        self.map.iter().filter(|tile| **tile == Tile::Bug).count()
+    }
+
+    fn layer(&self, layer: i32) -> Eris {
+        let area = self.height * self.width;
+        let layer = (((self.layers as i32) / 2) + layer) as usize;
+        Eris {
+            map: self.map[(layer * area)..((1 + layer) * area)].iter().cloned().collect(),
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
+
 fn main() {
     let mut eris = Eris::from_stdin();
+    let mut multi = MultiEris::new(&eris, 200);
+
     let mut seen = HashMap::new(); // {bio: iteration}
     for i in 0.. {
         if let Some(_) = seen.insert(eris.biodiversity_rating(), i) {
@@ -126,4 +266,9 @@ fn main() {
         }
         eris.tick();
     }
+
+    for _ in 0..200 {
+        multi.tick();
+    }
+    println!("{}", multi.count_live());
 }
