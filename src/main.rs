65c15b1acd474c0ee4ac2ce081803d4f56700d97
Lonami Exo <totufals@hotmail.com>
2020-08-29 14:17:47 +0200

    Generate image files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufWriter, Write};
+
+const IMAGE_WIDTH: usize = 128;
+const IMAGE_HEIGHT: usize = 128;
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    write!(stdout, "P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT)?;
+
+    for i in (0..IMAGE_HEIGHT).rev() {
+        for j in (0..IMAGE_WIDTH).rev() {
+            let r = i as f64 / (IMAGE_HEIGHT as f64 - 1.0);
+            let g = j as f64 / (IMAGE_WIDTH as f64 - 1.0);
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            write!(stdout, "{} {} {}\n", ir, ig, ib)?;
+        }
+    }
+
+    Ok(())
 }
