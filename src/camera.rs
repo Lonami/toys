8ab7012770877af8dc02d625a97975eab48d2532
Lonami Exo <totufals@hotmail.com>
2020-08-29 20:19:25 +0200

    Allow setting the camera position

--- src/camera.rs
+++ src/camera.rs
@@ -9,18 +9,21 @@ pub struct Camera {
 
 impl Camera {
     /// vfov is the vertical field-of-view in degrees.
-    pub fn new(vfov: f64, aspect_ratio: f64) -> Self {
+    /// vup is the view-up vector, generally the world up.
+    pub fn new(look_from: Vec3, look_at: Vec3, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Self {
         let theta = vfov.to_radians();
         let h = (0.5 * theta).tan();
         let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
 
-        let origin = Vec3::new(0.0, 0.0, 0.0);
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner =
-            origin - 0.5 * horizontal - 0.5 * vertical - Vec3::new(0.0, 0.0, focal_length);
+        let w = (look_from - look_at).unit();
+        let u = vup.cross(w).unit();
+        let v = w.cross(u);
+
+        let origin = look_from;
+        let horizontal = viewport_width * u;
+        let vertical = viewport_height * v;
+        let lower_left_corner = origin - 0.5 * horizontal - 0.5 * vertical - w;
 
         Self {
             origin,
@@ -30,10 +33,10 @@ impl Camera {
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray::new(
             self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
         )
     }
 }
