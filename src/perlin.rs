a7d85c721adfc8b4f180e34700fcc2ca1f95639f
Lonami Exo <totufals@hotmail.com>
2020-09-06 10:49:07 +0200

    Smooth perlin noise with trilinear interpolation

--- src/perlin.rs
+++ src/perlin.rs
@@ -2,6 +2,7 @@ use crate::{rand_f64, rand_u64, Vec3};
 
 // Must be a power of 2.
 const POINT_COUNT: usize = 256;
+const POINT_MASK: usize = POINT_COUNT - 1;
 
 pub struct Perlin {
     values: Vec<f64>,
@@ -23,15 +24,28 @@ impl Perlin {
     }
 
     pub fn noise(&self, point: Vec3) -> f64 {
-        let _u = point.x.fract();
-        let _v = point.y.fract();
-        let _w = point.z.fract();
+        // Note this is not the same as `f64::fract` for negative numbers
+        let u = point.x - point.x.floor();
+        let v = point.y - point.y.floor();
+        let w = point.z - point.z.floor();
 
-        let i = (4.0 * point.x) as usize & (POINT_COUNT - 1);
-        let j = (4.0 * point.y) as usize & (POINT_COUNT - 1);
-        let k = (4.0 * point.z) as usize & (POINT_COUNT - 1);
+        let i = point.x.floor() as isize;
+        let j = point.y.floor() as isize;
+        let k = point.z.floor() as isize;
+        let mut c = [[[0.0; 2]; 2]; 2];
 
-        self.values[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.values[self.perm_x
+                        [(i + di as isize) as usize & POINT_MASK]
+                        ^ self.perm_y[(j + dj as isize) as usize & POINT_MASK]
+                        ^ self.perm_z[(k + dk as isize) as usize & POINT_MASK]]
+                }
+            }
+        }
+
+        trilinear_interp(c, u, v, w)
     }
 
     fn generate_perm() -> Vec<usize> {
@@ -45,4 +59,23 @@ impl Perlin {
 
         perm
     }
+}
+
+fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let mut accum = 0.0;
+    for i in 0..2 {
+        let fi = i as f64;
+        for j in 0..2 {
+            let fj = j as f64;
+            for k in 0..2 {
+                let fk = k as f64;
+                accum += c[i][j][k]
+                    * (fi * u + (1.0 - fi) * (1.0 - u))
+                    * (fj * v + (1.0 - fj) * (1.0 - v))
+                    * (fk * w + (1.0 - fk) * (1.0 - w));
+            }
+        }
+    }
+
+    accum
 }
